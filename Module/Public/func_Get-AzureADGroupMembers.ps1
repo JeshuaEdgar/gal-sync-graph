248061a2e04794c536d119b3f8287305222ca387
Jeshua Edgar <jeshuaedgar@icloud.com>
2023-07-07 17:30:36 +0200

    get AAD group members, works for listed groups

---
+++ Module/Public/func_Get-AzureADGroupMembers.ps1
@@ -0,0 +1,32 @@
+function Get-AzureADGroupMembers {
+    param (
+        [CmdletBinding()]
+
+        [Parameter(Mandatory)]
+        [string]$Name
+    )
+    try {
+        $userList = @()
+        $groupList = New-GraphRequest -Method Get -Endpoint ("/groups?`$filter=startswith(displayName,'{0}')" -f $Name)
+        if ($groupList) {
+            do {
+                foreach ($group in $groupList) {
+                    Write-Host $group.mailNickname
+                    $users = New-GraphRequest -Method Get -Endpoint "/groups/$($group.id)/members"
+                    $userList += $users | Where-Object { $_."@odata.type" -eq "#microsoft.graph.user" }
+                    $groups = $users | Where-Object { $_."@odata.type" -eq "#microsoft.graph.group" }
+                }
+                $groupList = $groups
+            } until (
+                ($users | Where-Object { $_."@odata.type" -eq "#microsoft.graph.group" }).count -eq 0
+            )
+            return $userList
+        }
+        else {
+            Write-Host "No users found in $Name"
+        }
+    }
+    catch {
+        throw $(Format-ErrorCodes $_).ErrorMessage
+    }
+}
