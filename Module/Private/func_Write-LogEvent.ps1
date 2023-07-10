15cd87297a13a5e1d8386e39d77ea9731c7446e0
Jeshua Edgar <jeshuaedgar@icloud.com>
2023-07-10 11:13:19 +0200

    simple log event outputter

---
+++ Module/Private/func_Write-LogEvent.ps1
@@ -0,0 +1,11 @@
+function Write-LogEvent {
+    param (
+        [validateset("Info", "Error", "Debug")][string]$Level = "Info",
+        [parameter(Mandatory)][string]$Message
+    )
+    $timeStamp = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+    
+    $logOutput = $timeStamp, $Level, $Message -join " | "
+
+    Write-Output $logOutput
+}
