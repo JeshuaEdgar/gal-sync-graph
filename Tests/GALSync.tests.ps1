ae48a75a4871e9076eb98fd1d8f408c647bd5bfa
Jeshua Edgar <jeshuaedgar@icloud.com>
2023-07-18 19:17:40 +0200

    fix github actions

--- Tests/GALSync.tests.ps1
+++ Tests/GALSync.tests.ps1
@@ -1,24 +1,21 @@
+param (
+    [parameter(Mandatory)]$applicationID,
+    [parameter(Mandatory)]$applicationSecret,
+    [parameter(Mandatory)]$tenantID
+)
 BeforeAll {
-    $moduleRoot = (Get-Item $PSScriptRoot).Parent.FullName
-    $devSettings = Get-Content "$moduleRoot\Module\Tools\settings.local.json" | ConvertFrom-Json
-    $validKeys = @("applicationID", "applicationSecret", "tenant")
-    ForEach ($key in $devSettings.PSObject.Properties.Name) {
-        if ($validKeys -Contains $key) {
-            [Environment]::SetEnvironmentVariable($key, $devSettings.$key)
-        }
-    }
     Import-Module "$moduleRoot\Module\Gal-Sync.psd1" -Force
 }
 
 Describe "gal-sync-graph" {
     It "Should Connect" {
-        $credObject = New-Object System.Management.Automation.PSCredential ($env:applicationID, ($env:applicationSecret | ConvertTo-SecureString -AsPlainText -Force))
-        Connect-GALSync -Credential $credObject -Tenant $env:tenant | Should -BeTrue
+        $credObject = New-Object System.Management.Automation.PSCredential ($applicationID, ($applicationSecret | ConvertTo-SecureString -AsPlainText -Force))
+        Connect-GALSync -Credential $credObject -Tenant $tenantID | Should -BeTrue
     }
     It "Should return more than 1 GAL contacts" {
         (Get-GALContacts).Count | Should -BeGreaterThan 1
     }
-    It "Should return 'U.S. Sales' members (17)" {
+    It "Should return 17 members" {
         (Get-GALAADGroupMembers -Name "U.S. Sales").Count | Should -Be 17
     }
     It "Should return Contacts folder of user" {
