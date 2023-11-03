bfde65366de821b3938e37aa0cad7086968790e4
Jussi Hietanen <jussi.hietanen@iki.fi>
2023-11-03 10:33:42 +0200

    Fix bug with multiple Contact folders with name $ContactFolderName
    This commit fixes a bug which was caused if there were multiple
    Contact folders with the desired sync folder name. In such case
    the Get-ContactFolder would return a list of objects and for that
    reason the following Graph API requests would fail.
    
    This bug was discussed in #3

--- Module/Public/ContactFolder/func_Get-ContactFolder.ps1
+++ Module/Public/ContactFolder/func_Get-ContactFolder.ps1
@@ -12,13 +12,25 @@ function Get-ContactFolder {
             $folderList = $folderList | Where-Object { $_.wellKnownName }
         }
         else { # if ContactFolderName is not filled in
-            $folderList = $folderList | Where-Object { $_.displayName -like $ContactFolderName }
+            $folderList = $folderList | Where-Object { $_.displayName -eq $ContactFolderName }
         }
         if (-not $folderList) {
             Write-VerboseEvent "Not able to find the contact folder $ContactFolderName for $Mailbox"
             return $false | Out-Null
         }
-        Write-VerboseEvent "Found folder $ContactFolderName, returning!"
+
+        # If there are multiple contact folders present with the same name,
+        # we try to delete every one of them, return false and re-create the sync 
+        # folder with the name $ContactFolderName later
+        if ($folderList.Count -gt 1) {
+            ForEach($folderEntry in $folderList) {
+                Delete-ContactFolder -Mailbox $Mailbox -ContactFolder $folderEntry
+            }
+            Write-VerboseEvent -Message "Sleeping a few seconds to wait for API update"
+            Start-Sleep (Get-Random -Minimum 5 -Maximum 15)
+            return $false | Out-Null
+        }
+
         $folderList | Add-Member -MemberType NoteProperty -Name "mailBox" -Value $Mailbox
         return $folderList
     }
